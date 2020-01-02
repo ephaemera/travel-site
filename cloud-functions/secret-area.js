8e40f2762f831b158ad6c4be7d43c0ba0898d3e0
ephaemera <ephaemera@gmail.com>
2020-01-02 09:48:29 -0800

    Updated cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,7 @@
 exports.handler = function (event, context, callback) {
-  const secretContent = <h3>Welcome To The Secret Area</h3> 
-  <p> Here we can tell you the sky is <strong>blue</strong>,
-    and two plus two equals four.</p>
+  const secretContent = `<h3>Welcome To The Secret Area</h3> 
+  <p>Here we can tell you the sky is <strong>blue</strong>,
+    and two plus two equals four.</p>`
 
 
   let body
